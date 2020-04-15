3e7de91aa71aa2ea5edef5f857dfdd08ed893e88
Ankit Sinha <40132871+ankit19sinha@users.noreply.github.com>
2020-04-14 21:24:30 -0500

    Add files via upload
    Shell script for running RNNs with attention mechanism

---
+++ run_attnRNN.sh
@@ -0,0 +1,12 @@
+python -u train.py \
+    --model attention \
+    --epochs 5 \
+    --lr 0.01 \
+    --weight-decay 0.0 \
+    --batch-size 128 \
+    --hidden_dim 100 \
+    --useGlove True \
+    --trainable True \
+    --bidirectional True \
+    --typeOfRNN simple \
+    --typeOfPadding no_padding | tee attnRNN.log

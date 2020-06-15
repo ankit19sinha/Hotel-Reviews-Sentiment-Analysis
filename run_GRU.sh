422db0f1f0df334c047cb220697cf7d5b54be30f
Ankit Sinha <40132871+ankit19sinha@users.noreply.github.com>
2020-06-15 11:17:45 -0500

    Update run_GRU.sh

--- run_GRU.sh
+++ run_GRU.sh
@@ -3,11 +3,11 @@ python -u train.py \
     --epochs 5 \
     --lr 0.01 \
     --weight-decay 0.0 \
-    --batch-size 128 \
+    --batch-size 500 \
     --hidden_dim 100 \
-    --dropout 0.3 \
-    --num_layers 2 \
-    --useGlove True \
+    --dropout 0.0 \
+    --num_layers 1 \
+    --useGlove False \
     --trainable False \
     --bidirectional False \
-    --typeOfPadding padded | tee GRU.log
+    --typeOfPadding pack_padded | tee GRU.log
